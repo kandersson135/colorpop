956246679e99d4056dcbb168db0db4b7f09f3259
kandersson135 <115895961+kandersson135@users.noreply.github.com>
2024-07-01 20:27:06 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -354,7 +354,7 @@ $(document).ready(function() {
 
   // How-to-button
   $('#how-to-button').click(function() {
-    swal("Game instructions", "ColorPop! is a colorful puzzle game. Click on groups of three or more connected dots of the same color to remove them and earn points. The more dots you remove in one click, the higher your score. When dots are removed, the dots above fall down, and dots shift to the center to fill gaps, keeping the board compact. \n\n Clear all the dots to advance to the next round with your score intact. If no more valid moves are left, the game is over, and you can save your score. Aim to beat your high score and enjoy the challenge!");
+    swal("Game instructions", "ColorPop! is a colorful puzzle game. Click on groups of three or more connected dots of the same color to remove them and earn points. The more dots you remove in one click, the higher your score. \n\n Clear all the dots to advance to the next round with your score intact. If no more valid moves are left, the game is over, and you can save your score. Aim to beat your high score and enjoy the challenge!");
   });
 
   // Reset leaderboard button click
